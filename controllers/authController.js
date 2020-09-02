6ee02eebdbae23dba7df8852cac812e577cf8d05
kultosh <kulgole90@gmail.com>
2020-09-02 22:24:19 +0545

    Learn to create validation along with error handling

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,18 +1,57 @@
 const User = require('../models/User');
 
+// handle errors
+const handleErrors = (err) => {
+    console.log(err.message, err.code);
+    let error = {
+        email: '',
+        password: ''
+    };
+
+    // duplicate error code
+    if(err.code === 11000)
+    {
+        error.email = 'That email is already registered!';
+        return error;
+    }
+
+    // validation errors
+    if (err.message.includes('user validation failed')) {
+        // console.log(Object.values(err.errors).forEach(error => {
+        //     console.log(error.properties.message);
+        // }));
+
+        Object.values(err.errors).forEach(({
+            properties
+        }) => {
+            error[properties.path] = properties.message;
+        })
+    }
+
+    return error;
+}
+
 const signup_create = (req, res) => {
     res.render('signup');
 }
 
-const signup_store = async(req, res) => {
-    const {email, password} = req.body;
+const signup_store = async (req, res) => {
+    const {
+        email,
+        password
+    } = req.body;
 
     try {
-        const user = await User.create({email, password});
+        const user = await User.create({
+            email,
+            password
+        });
         res.status(201).json(user);
-    } catch (error) {
-        console.log(error);
-        res.status(400).send('error, user not created');
+    } catch (err) {
+        const errors = handleErrors(err);
+        res.status(400).json({
+            errors
+        });
     }
 }
 
@@ -21,7 +60,10 @@ const login_get = (req, res) => {
 }
 
 const login_post = (req, res) => {
-    const {email, password} = req.body;
+    const {
+        email,
+        password
+    } = req.body;
     console.log(email, password);
     res.send('user login')
 }
