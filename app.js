69b657f0e8fb58838cfd12fdbcbe89ae44026d53
kultosh <kulgole90@gmail.com>
2020-09-03 15:34:38 +0545

    Install jasonwebtoken for the jwt authentication

--- app.js
+++ app.js
@@ -34,18 +34,3 @@ app.get('/smoothies', (req, res) => {
 })
 
 app.use(authRoutes);
-
-// cookies
-app.get('/set-cookies', (req, res) => {
-    // res.setHeader('Set-Cookie', 'newUser=true');
-    res.cookie('newUser', false);
-    res.cookie('isEmployee', true, { maxAge: 1000 * 60 * 60 * 24, httpOnly: true });
-    res.send('Here goes your cookie!');
-});
-
-app.get('/read-cookies', (req, res) => {
-    const cookies = req.cookies;
-    console.log(cookies.newUser);
-
-    res.json(cookies);
-})
