61a9096aa1f856bfb9a17c8361b618a8be7cd44b
kultosh <kulgole90@gmail.com>
2020-09-06 22:06:11 +0545

    Created middleware to authenticate the user using jwt

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
 const cookieParser = require('cookie-parser');
+const { requireAuth } = require('./middleware/authMiddleware');
 
 const app = express();
 
@@ -29,7 +30,7 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 
-app.get('/smoothies', (req, res) => {
+app.get('/smoothies', requireAuth, (req, res) => {
     res.render('smoothies');
 })
 
